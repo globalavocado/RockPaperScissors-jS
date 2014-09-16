33b0554fbd075b1b482a59791267b51ddc116c3b
globalavocado <globalavocado@gmail.com>
2014-09-16 11:17:25 +0100

    added victory message, changed hash to deliver victory verbs,  all tests passed

--- javascript/lib/Game.js
+++ javascript/lib/Game.js
@@ -1,5 +1,7 @@
+
+
 Game.prototype.PAIRS = {
-    rock:     { beats: ["scissors", "lizard"] },
+    rock:     { scissors: 'crushes', lizard: 'crushes' },
     paper:    { rock: 'covers', spock: 'disproves' },
     scissors: { paper: 'cuts', lizard: 'decapitates' },
 	lizard:   { spock: 'poisons', paper: 'eats' },
@@ -12,19 +14,36 @@ Game.prototype.PAIRS = {
 };
 
 Game.prototype.winner = function(){
-	if (this.isSamePick()) return null
+	if (this.isSamePick()) return null;
 
-	if (this.PAIRS[this.player1.pick].beats.indexOf(this.player2.pick)(>-1){
-      return this.player1;
-  }
-    else
-      {return this.player2;}
+	if (this._victoryVerbFor(this.player1.pick, this.player2.pick)){
+      	return this.player1;
+  		}
+    else {
+     	return this.player2;
+ 		}
 };
+
+Game.prototype._victoryVerbFor = function(pick, opponentPick) {
+  return this.PAIRS[pick][opponentPick];
+}
+
+Game.prototype.loser = function(){
+	return (this.winner() === this.player1 ? this.player2 : this.player1);
+}
 
 Game.prototype.isSamePick = function(){
     return this.player1.pick === this.player2.pick; 
  };
 
- Game.prototype.winningMessage = function(){
- 	return 'Alex crushes Mario';
+Game.prototype.winningMessage = function(){
+ 	var message;
+ 	if (this.winner()){
+ 		message = [this.winner().name, 
+ 		this._victoryVerbFor(this.winner().pick, this.loser().pick), 
+ 		this.loser().name].join(' '); 		
+ 	} else {
+ 		message = 'Draw';
+ 	}
+ 	return message;
  };
--- javascript/lib/Player.js
+++ javascript/lib/Player.js
@@ -2,6 +2,6 @@ function Player(name){
 	this.name = name;
 };
 
-Player.prototype.picks = function(pick){
+Player.prototype.picks = function(pick) {
 	this.pick = pick;
 };
