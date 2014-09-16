5595f7e2dce8e0c43343f0ef629b4e5bf0573868
globalavocado <globalavocado@gmail.com>
2014-09-16 09:30:02 +0100

    separated the Player.js & Game.js files

--- javascript/lib/Game.js
+++ javascript/lib/Game.js
@@ -1,29 +1,22 @@
 Game.prototype.PAIRS = {
-    rock:     { scissors:'crushes', lizard: 'squashes' },
+    rock:     { beats: ["scissors", "lizard"] },
     paper:    { rock: 'covers', spock: 'disproves' },
     scissors: { paper: 'cuts', lizard: 'decapitates' },
 	lizard:   { spock: 'poisons', paper: 'eats' },
 	spock: 	  { rock: 'vapourises', scissors: 'smashes' }
   };
 
-function Player(name){
-	this.name = name;
-};
-
-function Game(player1, player2){
+  function Game(player1, player2){
 	this.player1 = player1;
 	this.player2 = player2;
-};
-
-Player.prototype.picks = function(pick){
-	this.pick = pick;
 };
 
 Game.prototype.winner = function(){
 	if (this.isSamePick()) return null
 
-	if (this.PAIRS[this.player1.pick].beats === this.player2.pick)
-      {return this.player1;}
+	if (this.PAIRS[this.player1.pick].beats.indexOf(this.player2.pick)(>-1){
+      return this.player1;
+  }
     else
       {return this.player2;}
 };
@@ -35,4 +28,3 @@ Game.prototype.isSamePick = function(){
  Game.prototype.winningMessage = function(){
  	return 'Alex crushes Mario';
  };
-
